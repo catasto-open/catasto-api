a11bd859adb531e271b3553422ce38c89239db86
Antonio Cerciello <antonio.cerciello@gmail.com>
2023-04-05 17:34:41 +0200

    Adding Visura JSON endpoint

--- app/routers/catastoprints.py
+++ app/routers/catastoprints.py
@@ -8,7 +8,7 @@ from app.services.pdfformatter import generate_print_pdf
 from app.services.csvformatter import generate_print_csv
 from app.config.database import get_db
 from app.config.auth import OpenAMIDToken, auth
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
 from datetime import date
 from enum import Enum
 import re
@@ -373,3 +373,54 @@ async def persone_giuridiche(
         raise HTTPException(
             status_code=404, 
             detail="Nessun risultato trovato")
+
+@router.get("/dati/visura", response_class=JSONResponse)
+async def visuraJSON(
+    utente: OpenAMIDToken = Security(auth.authorized),
+    comune: Optional[str] = Query(
+        "H501",
+        title="Comune",
+        description="Ricerca per codice immobile",
+        regex="^[a-zA-Z0-9_]*$",
+        max_length=4
+    ),
+    tipoimmobile: str = Query(
+        None,
+        title="Tipo Immobile",
+        description="Terreno (T) o Fabbricato (F)",
+        regex="^[a-zA-Z]*$",
+        max_length=1
+    ),
+    codiceimmobile: int = Query(
+        None,
+        title="Codice Immobile",
+        description="Ricerca per codice immobile"
+    ),
+    flagricercastorica: bool = Query(
+        False,
+        title="Flag ricerca storica",
+        description="Ricerca nei dati storici"
+    ),
+    db: get_db = Depends()):
+
+        # username = utente.sub
+
+        if codiceimmobile:
+            result = VisuraService(db).get_visura_by_codiceimmobile(flagricercastorica, comune, codiceimmobile, tipoimmobile)
+        else:
+            raise HTTPException(
+                    status_code=422, 
+                    detail="Dati input non validi")
+
+        if result:
+            # try:
+            #     return generate_print_pdf("Visura", result, username, data_aggiornamento)
+            # except BaseException:
+            #     raise HTTPException(
+            #         status_code=500, 
+            #         detail="Template non trovato")
+            return result
+        else:
+            raise HTTPException(
+                status_code=404, 
+                detail="Nessun risultato trovato")
